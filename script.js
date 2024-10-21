bf61b3ff90670b69bd9af5ffa6ede4c0d078cbf7
Kamen <nemak1862000@gmail.com>
2024-10-21 22:17:21 +0300

    made some fixes to possitioning

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function scrollToNextSection() {
+  const sections = document.querySelectorAll(".section");
+
+  // Find the current section
+  let currentSectionIndex = -1;
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+
+    if (
+      rect.top <= window.innerHeight / 2 &&
+      rect.bottom >= window.innerHeight / 2
+    ) {
+      currentSectionIndex = index;
+    }
+  });
+
+  // Scroll to the next section
+  if (currentSectionIndex !== -1 && currentSectionIndex < sections.length - 1) {
+    sections[currentSectionIndex + 1].scrollIntoView({ behavior: "smooth" });
+  }
 }
