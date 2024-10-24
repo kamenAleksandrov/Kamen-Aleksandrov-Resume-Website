bb7401958289eb0f8812d4c1da12b35e56ef1578
Kamen <nemak1862000@gmail.com>
2024-10-24 19:04:44 +0300

    reordered some css edited the design a bit

--- script.js
+++ script.js
@@ -70,3 +70,28 @@ document.addEventListener("DOMContentLoaded", () => {
     themeIcon.title = "Switch to Dark Mode";
   }
 });
+
+// Fade in animation
+document.addEventListener("DOMContentLoaded", function() {
+  const faders = document.querySelectorAll('.fade-in');
+
+  const appearOptions = {
+      threshold: 0.1,
+      rootMargin: "0px 0px -100px 0px"
+  };
+
+  const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+      entries.forEach(entry => {
+          if (!entry.isIntersecting) {
+              return;
+          } else {
+              entry.target.classList.add('fade-in-visible');
+              appearOnScroll.unobserve(entry.target);
+          }
+      });
+  }, appearOptions);
+
+  faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+  });
+});
